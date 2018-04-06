98ff2fe5e7711c2b8ecda1f02e25aa16b789315d
Luis CRUZ <38135917+cruzl@users.noreply.github.com>
2018-04-06 18:03:38 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# TRACES
+Traces library in C language for embedded software.
+
+
+## Description
+
+This library aims to...
+
+
+## Format
+
+Trace:
+> <file_name>:<line_number>:<function_name>    <Message>
+
+Error:
+> <file_name>:<line_number>:<function_name>    ERROR: <Message>
+
+
+## Usage
+
+Example:
+```c {.line-numbers}
+#define DEBUG
+
+#import "traces.h"
+
+void
+main ( void )
+{
+    // Print a trace
+    TRACE( "This is a trace" );
+
+    // Print a trace with parameters
+    TRACE_P( "This is a trace with %d parameter" , 1 );
+
+    // Print an error trace
+    ERROR( "This is an error trace" );
+
+    // Print a trace with parameters
+    ERROR_P( "This is an error trace with %d parameter" , 1 );
+}
+```
+
+Result:
+> demo.c:9:main()    This is a trace
+> demo.c:11:main()    This is a trace with 1 parameter
+> demo.c:13:main()    ERROR: This is an error trace
+> demo.c:15:main()    ERROR: This is an error trace with 1 parameter
---
+++ traces.h
@@ -0,0 +1,59 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2018 Luis CRUZ, Traces
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef __TRACES_H__
+#define __TRACES_H__
+
+
+
+// -----------------------------------------------
+//     INCLUDE
+// -----------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+// -----------------------------------------------
+//     MACRO
+// -----------------------------------------------
+
+#ifdef DEBUG
+#define __TRACE     1
+#else
+#define __TRACE     0
+#endif
+
+#define PRINT_TRACE( ... ) \
+    do { if ( __TRACE ) { fprintf( stderr , "%s:%d:%s()    " , __FILE__ , __LINE__ , __func__ ); fprintf( stderr , __VA_ARGS__ ); fprintf( stderr , "\r\n" ); } } while ( 0 )
+
+#define TRACE( format )                 PRINT_TRACE( format );
+#define TRACE_P( format , ... )         PRINT_TRACE( format , __VA_ARGS__ );
+#define ERROR( format )                 PRINT_TRACE( "ERROR: " format );
+#define ERROR_P( format , ... )         PRINT_TRACE( "ERROR: " format , __VA_ARGS__ );
+
+
+
+#endif /* __TRACES_H__ */
